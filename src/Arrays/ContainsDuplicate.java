3a2b15628cbb55201e55a84725bd93e764dfffba
rohith <rohith.officia@gmail.com>
2025-09-01 19:18:33 +0530

    containsDuplicate

---
+++ src/Arrays/ContainsDuplicate.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/contains-duplicate?envType=problem-list-v2&envId=array
+
+package Arrays;
+
+import java.util.HashSet;
+//import java.util.Set;
+
+public class ContainsDuplicate {
+    public static boolean contains_Duplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i : nums){
+            if(!set.contains(i)){
+                set.add(i);
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String [] args){
+        int [] arr = {7,1,5,3,6,4};
+        System.out.print(contains_Duplicate(arr));
+    }
+}
