dd3a6eac7d133f766304b091af88acd05fc7c9b4
rohith <rohith.officia@gmail.com>
2025-09-17 21:22:14 +0530

    Power of Two

---
+++ src/Math/PowerofTwo.java
@@ -0,0 +1,12 @@
+package Math;
+
+public class PowerofTwo {
+    public static boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    public static void main(String [] args){
+        System.out.println(isPowerOfTwo(1)); // true
+        System.out.print(isPowerOfTwo(3)); // false
+        }
+}
